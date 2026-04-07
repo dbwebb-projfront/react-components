3081498306669a4f307d6089e170d3eca2690c85
Emil Folino <emilfolino@gmail.com>
2026-04-07 10:27:53 +0200

    before lecture

---
+++ web-components/main.js
@@ -0,0 +1,5 @@
+import AppComponent from "./components/app-component.js"
+import CounterComponent from "./components/counter-component.js"
+
+customElements.define("app-component", AppComponent)
+customElements.define("counter-component", CounterComponent)
